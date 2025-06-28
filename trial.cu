169b30eb3bf4a6434eacdb183fe49b48b271adfa
Sahilgill24 <gursahilgill24@gmail.com>
2025-06-28 05:38:18 +0530

    working kernel

--- trial.cu
+++ trial.cu
@@ -4,12 +4,16 @@
 #include <vector>
 #include <iostream>
 #include <cassert>
+#include <cstdlib>
+#include <cstdio>
 
 // Similiar to Matrix Multiplication
 // There we use a 2D grid and 2D blocks
 // O(bcd), so can be parallelized
 #define HOST_DEVICE_INLINE __host__ __device__ __forceinline__
 #define HOST_INLINE __host__ __forceinline__
+
+#define P_MOD 32843
 template <unsigned P = P_MOD>
 
 class Dummy_Scalar
@@ -67,18 +71,69 @@ public:
 
 typedef Dummy_Scalar<> test_scalar;
 
-__global__ void horner_kernel() {}
+__global__ void horner_kernel(const test_scalar *coeffs,
+                              const test_scalar *domain,
+                              int coeffs_size,
+                              int domain_size,
+                              int batch_size,
+                              test_scalar *evals)
+{
+    int idx_in_batch = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx_in_batch >= batch_size)
+        return;
 
-void polyeval(
-    const T *coeffs,
-    const T *domain,
+    const test_scalar *curr_coeffs = coeffs + idx_in_batch * coeffs_size;
+    test_scalar *curr_evals = evals + idx_in_batch * domain_size;
+
+    for (int eval_idx = 0; eval_idx < domain_size; ++eval_idx)
+    {
+        curr_evals[eval_idx] = curr_coeffs[coeffs_size - 1];
+        for (int coeff_idx = coeffs_size - 2; coeff_idx >= 0; --coeff_idx)
+        {
+            curr_evals[eval_idx] =
+                curr_evals[eval_idx] * domain[eval_idx] + curr_coeffs[coeff_idx];
+        }
+    }
+}
+// one includes the kernel without the polynomials being batched.
+// for (uint64_t idx_in_batch = 0; idx_in_batch < batch_size; ++idx_in_batch)
+// {
+//     const Scalar *curr_coeffs = coeffs + idx_in_batch * coeffs_size;
+//     Scalar *curr_evals = evals + idx_in_batch * domain_size;
+//     for (uint64_t eval_idx = 0; eval_idx < domain_size; ++eval_idx)
+//     {
+//         curr_evals[eval_idx] = curr_coeffs[coeffs_size - 1];
+//         for (int64_t coeff_idx = coeffs_size - 2; coeff_idx >= 0; --coeff_idx)
+//         {
+//             curr_evals[eval_idx] =
+//                 curr_evals[eval_idx] * domain[eval_idx] + curr_coeffs[coeff_idx];
+//         }
+//     }
+// }
+__global__ void horner_kernel2() {}
+
+void poly_eval(
+    const test_scalar *coeffs,
+    const test_scalar *domain,
     int coeffs_size,
     int domain_size,
     int batch_size,
-    T *evals /*OUT*/)
+    test_scalar *evals /*OUT*/)
 {
+    dim3 grid_size((batch_size + 255) / 256);
+    dim3 block_size(256);
+    horner_kernel<<<grid_size, block_size>>>(coeffs, domain, coeffs_size, domain_size, batch_size, evals);
+}
 
-    
+void polyeval2(
+    const test_scalar *coeffs,
+    const test_scalar *domain,
+    int coeffs_size,
+    int domain_size,
+    int batch_size,
+    test_scalar *evals /*OUT*/)
+{
+    // using the kernel here
 }
 
 int main()
@@ -87,10 +142,10 @@ int main()
     float time;
 
     // test parameters
-    int coeffs_log_size = (argc > 1) ? atoi(argv[1]) : 22;
+    int coeffs_log_size = 22;
     int coeffs_size = 1 << coeffs_log_size;
-    int domain_size = (argc > 2) ? atoi(argv[2]) : 7;
-    int batch_size = (argc > 3) ? atoi(argv[3]) : 10;
+    int domain_size = 7;
+    int batch_size = 10;
     int total_coeffs_size = batch_size * coeffs_size;
     int total_results_size = batch_size * domain_size;
 
@@ -127,8 +182,7 @@ int main()
 
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+
 
     // warm up
     poly_eval(d_coeffs, d_domain, coeffs_size, domain_size, batch_size, d_results);
@@ -142,7 +196,6 @@ int main()
     printf("runtime : %.3f ms.\n", time);
 
     // run reference and check correctness
-    poly_eval_ref(coeffs, domain, coeffs_size, domain_size, batch_size, ref_results);
 
     cudaMemcpy(results, d_results, sizeof(test_scalar) * total_results_size, cudaMemcpyDeviceToHost);
 
